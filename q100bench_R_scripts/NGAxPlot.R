4a86386bba788a32ce7d4932c16e990c7005060f
Nancy F. Hansen <nhansen@mail.nih.gov>
2025-06-12 11:58:55 -0400

    Fixing haploid/diploid issue

--- q100bench_R_scripts/NGAxPlot.R
+++ q100bench_R_scripts/NGAxPlot.R
@@ -7,7 +7,7 @@ methodcolors <- c("#DDCC77", "#661100", "#6699CC")
 assemblyname <- ifelse(!( is.na(args[1])), args[1], "lc24_ul_herro_ulk")
 genomename <- ifelse(!( is.na(args[2])), args[2], "v1.1")
 outputdir <- ifelse(!( is.na(args[3])), args[3], ".")
-idealfile <- ifelse(!( is.na(args[4])), args[4], "hg002v1.1.alignclusterlengths.txt")
+idealfile <- ifelse(!( is.na(args[4])), args[4], "v1.1.non_n_seq.bed")
 plottitle <- ifelse(!( is.na(args[5])), args[5], paste(c("NGAx ", assemblyname, " vs ", genomename), sep="", collapse=""))
 lengthtypes <- c("alignclusterlengths", "contiglengths", "scaffoldlengths")
 assemblysizefiles <- sapply(lengthtypes, function(x) {file=paste(c(outputdir, "/", assemblyname, ".", x, ".txt"), sep="", collapse=""); return(file)})
@@ -20,8 +20,19 @@ readlengths <- function(sizefile) {
   return(clusterlengths)
 }
 
+readideallengths <- function(bedfile) {
+  idealdf <- read.table(bedfile)
+  idealdf$lengths <- (as.integer(idealdf$V3)-as.integer(idealdf$V2))/1000000
+  
+  totallengths <- sum(idealdf$lengths)
+  sortedlengths <- sort(idealdf$lengths, decreasing=TRUE)
+  perccovered <- sapply(seq(1, length(sortedlengths)), function(i) {return(100*sum(sortedlengths[seq(1,i)])/totallengths)})
+  
+  return(data.frame(clusterlength=sortedlengths, perc=perccovered))
+}
+
 plotlengths <- function(clusterlengths, color="red", title="NGAx", dashed=FALSE, ltyval=NA, cexval=1.0) {
-  xcoords <- append(0, clusterlengths$perc/2)
+  xcoords <- append(0, clusterlengths$perc)
   ycoords <- c(clusterlengths$clusterlength, 0)
   if (is.na(ltyval)) {
     ltyval <- ifelse(dashed, 2, 1)
@@ -30,7 +41,7 @@ plotlengths <- function(clusterlengths, color="red", title="NGAx", dashed=FALSE,
 }
 
 addclusterlengths <- function(clusterlengths, color="blue", dashed=FALSE, ltyval=NA) {
-  xcoords <- append(0, clusterlengths$perc/2)
+  xcoords <- append(0, clusterlengths$perc)
   ycoords <- c(clusterlengths$clusterlength, 0)
   if (is.na(ltyval)) {
     ltyval <- ifelse(dashed, 2, 1)
@@ -59,7 +70,7 @@ assembly_ngax_plot <- function(clusterfiles, contigfiles=c(), scaffoldfiles=c(),
     }
   }
   if (ideal) {
-    ideallengths <- readlengths(idealfile)
+    ideallengths <- readideallengths(idealfile)
     addclusterlengths(ideallengths, col="black")
     assemblycolors <- c(assemblycolors[1:length(assemblylabels)], "black")
     assemblylabels <- c(assemblylabels, "Ideal (HG002v1.1)")
